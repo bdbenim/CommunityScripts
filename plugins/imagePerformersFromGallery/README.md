7b307f7805542000eff030798a4a88f09dc7a3a8
bdbenim <bdben@protonmail.com>
2022-12-24 01:39:43 -0800

    Added tag filtering and updated readme

--- plugins/imagePerformersFromGallery/README.md
+++ plugins/imagePerformersFromGallery/README.md
@@ -1,5 +1,31 @@
-This plugin searches for all images with a performer count of 0 and sets their performers to be the same as their parent
-gallery/galleries if the galleries have any performers.
+# Image Performers From Gallery
 
-Any images that are marked as organized or that already have 1 or more performers will be ignored, even if their gallery
-has more performers, since these are assumed to be already organized.
+This plugin sets image performers to match their gallery metadata without modifying already organized images.
+
+# Requirements
+
+- Stash (tested on v18.0)
+- Python 3 (tested on 3.10.8)
+- stashapi module (`pip install stashapp-tools`)
+
+# Usage
+
+## 'All Galleries' Task
+
+Any images that already have 1 or more performers will be ignored, even if their gallery
+has more performers, since these are assumed to be already organized. This can be
+overridden by creating a tag called "Update Performers" and applying it to images that
+you want to force an update on.
+
+## 'Tagged Galleries' Task
+
+Changes can be limited to specific galleries by applying the same "Update Performers"
+tag to them and running the "Tagged Galleries" task. Images not in these galleries
+will be ignored (even if they also have the "Update Performers" tag)
+
+## 'Dry Run' Task
+
+Running this task performs only the searching for galleries and images without making
+any changes. The number of images matching the criteria can be found in the stash logs.
+Currently this only corresponds to the 'All Galleries' task, so does not reflect what will
+happen when running the 'Tagged Galleries' task.
--- plugins/imagePerformersFromGallery/imagePerformersFromGallery.py
+++ plugins/imagePerformersFromGallery/imagePerformersFromGallery.py
@@ -9,16 +9,23 @@ except ModuleNotFoundError:
           file=sys.stderr)
 
 FRAGMENT = json.loads(sys.stdin.read())
-MODE = FRAGMENT['args']['mode']
 stash = StashInterface(FRAGMENT["server_connection"])
 
-GALLERY_FRAGMENT = "id"
+GALLERY_FRAGMENT = """
+id
+tags {
+    id
+}
+"""
 IMAGE_FRAGMENT = """
 id
 galleries {
     performers {
         id
     }
+}
+tags {
+    id
 }
 """
 
@@ -27,12 +34,28 @@ GALLERY_FILTER = {"performer_count": {"modifier": "NOT_EQUALS", "value": 0}}
 
 # Filter all images that are not organized and that have 0 performers.
 # This filter also includes gallery ids, which will be inserted later.
-IMAGE_FILTER = {"organized": False, "performer_count": {
-    "modifier": "EQUALS", "value": 0}, "galleries": {"value": [], "modifier": "INCLUDES"}}
+# The "Update Performers" tag can override ignored images.
+IMAGE_FILTER = {"galleries": {"value": [], "modifier": "INCLUDES"},
+    "AND": {"organized": False, "performer_count": { "modifier": "EQUALS", "value": 0}, 
+    "OR": {"tags":{"value": -1, "modifier": "INCLUDES"}}}}
 
 
 def main():
-    log.info(f"Running in mode {MODE}")
+    MODE = FRAGMENT['args']['mode']
+    log.info(f"Running in {MODE} mode")
+
+    update_tag_id = -1
+    try:
+        update_tag_id = stash.find_tag('Update Performers')["id"]
+        log.error(f"Tag ID is {update_tag_id}")
+        IMAGE_FILTER["AND"]["OR"]["tags"]["value"] = update_tag_id
+        if MODE == "tagged":
+            # Only consider galleries with the "Update Performers" tag
+            GALLERY_FILTER["tags"] = {"value": update_tag_id, "modifier": "INCLUDES"}
+            log.info(f"Using gallery filter {GALLERY_FILTER}")
+    except:
+        log.info('"Update Performers" tag does not exist, proceeding without tag filtering')
+
 
     # Step 1, find galleries with performers:
     log.info("Searching for galleries...")
@@ -46,32 +69,54 @@ def main():
     for i, gallery in enumerate(gallery_list):
         log.progress(i/total)
         galleryID = gallery["id"]
-        IMAGE_FILTER["galleries"]["value"] = [galleryID] # Include current gallery in the image filter
+        # Include current gallery in the image filter
+        IMAGE_FILTER["galleries"]["value"] = [galleryID]
         image_list = stash.find_images(IMAGE_FILTER, fragment=IMAGE_FRAGMENT)
         images = len(image_list)
         imagetotal += images
 
         # Step 3, set image performers:
-        if MODE == "update":
+        if MODE != "dryrun":
             for image in image_list:
                 performer_ids = []
                 # Images may belong to multiple galleries, which may or may not have
                 # the same performers. All performers from all galleries will
                 # be added to the image, not just the gallery from the filter.
-                for gallery in image["galleries"]:
-                    for performer in gallery["performers"]:
+                for imgGallery in image["galleries"]:
+                    for performer in imgGallery["performers"]:
                         id = performer["id"]
                         if id not in performer_ids:
                             performer_ids.append(id)
+                
+                # Get the tag ids for the image so that the Update tag can be removed
+                tagids = []
+                for tag in image["tags"]:
+                    tagid = tag["id"]
+                    if tagid == update_tag_id:
+                        continue # Remove update tag after image is processed
+                    tagids.append(tagid)
 
-                update_input = {"id": image["id"], "performer_ids": performer_ids}
-                stash.update_image(update_input)
-            log.info(f"Updated {len(image_list)} images from gallery {galleryID}.")
+                update_input = {"id": image["id"],
+                                "performer_ids": performer_ids,
+                                "tag_ids": tagids}
+                stash.update_image(update_input) # Save performers and tags
+            log.info(
+                f"Updated {len(image_list)} images from gallery {galleryID}.")
 
-    if MODE == "update":
+        # Step 4, remove Update tag from gallery:
+        tagids = []
+        for tag in gallery["tags"]:
+            tagid = tag["id"]
+            if tagid == update_tag_id:
+                continue
+            tagids.append(tagid)
+        update_input = {"id": galleryID, "tag_ids": tagids}
+        stash.update_gallery(update_input)
+
+    if MODE == "dryrun":
+        log.info(f"Found {imagetotal} eligible image(s). No changes were made.")
+    else:
         log.info(f"Finished updating {imagetotal} images.")
-    elif MODE == "dryrun":
-        log.info(f"Found {imagetotal} eligible images. No changes were made.")
 
     log.exit("Plugin exited normally.")
 
