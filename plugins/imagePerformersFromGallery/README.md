2d25f802152a5fceba46e91c729c14f443294516
bdbenim <bdben@protonmail.com>
2022-12-23 22:14:59 -0800

    Created plugin imagePerformersFromGallery

---
+++ plugins/imagePerformersFromGallery/README.md
@@ -0,0 +1,5 @@
+This plugin searches for all images with a performer count of 0 and sets their performers to be the same as their parent
+gallery/galleries if the galleries have any performers.
+
+Any images that are marked as organized or that already have 1 or more performers will be ignored, even if their gallery
+has more performers, since these are assumed to be already organized.
---
+++ plugins/imagePerformersFromGallery/imagePerformersFromGallery.py
@@ -0,0 +1,82 @@
+import json
+import sys
+
+try:
+    import stashapi.log as log
+    from stashapi.stashapp import StashInterface
+except ModuleNotFoundError:
+    print("You need to install the stashapi module. (pip install stashapp-tools)",
+          file=sys.stderr)
+
+FRAGMENT = json.loads(sys.stdin.read())
+MODE = FRAGMENT['args']['mode']
+stash = StashInterface(FRAGMENT["server_connection"])
+
+GALLERY_FRAGMENT = "id"
+IMAGE_FRAGMENT = """
+id
+galleries {
+    performers {
+        id
+    }
+}
+"""
+
+GALLERY_FILTER = {"performer_count": {"modifier": "NOT_EQUALS", "value": 0}}
+IMAGE_FILTER = {"organized": False, "performer_count": {
+    "modifier": "EQUALS", "value": 0}, "galleries": {"value": [], "modifier": "INCLUDES"}}
+
+
+def main():
+    # print("Filter:")
+    # print(IMAGE_FILTER["galleries"]["value"])
+    # stash.find_images()
+
+    # print("Plugin not working", file=sys.stderr)
+
+    log.info(f"Running in mode {MODE}")
+
+    # Step 1, find galleries with performers:
+    log.info("Searching for galleries...")
+    gallery_list = stash.find_galleries(
+        GALLERY_FILTER, fragment=GALLERY_FRAGMENT)
+    total = len(gallery_list)
+    log.info(f"Found {total} galleries")
+    # Step 2, find images in those galleries with no performers:
+    log.info("Searching for images...")
+    imagetotal = 0
+    for i, gallery in enumerate(gallery_list):
+        log.progress(i/total)
+        galleryID = gallery["id"]
+        IMAGE_FILTER["galleries"]["value"] = [galleryID]
+        image_list = stash.find_images(IMAGE_FILTER, fragment=IMAGE_FRAGMENT)
+        images = len(image_list)
+        imagetotal += images
+
+        # Step 3, set image performers:
+        if MODE == "update":
+            for image in image_list:
+                performer_ids = []
+                # Images may belong to multiple galleries, which may or may not have
+                # the same performers. All performers from all galleries will
+                # be added to the image.
+                for gallery in image["galleries"]:
+                    for performer in gallery["performers"]:
+                        id = performer["id"]
+                        if id not in performer_ids:
+                            performer_ids.append(id)
+
+                update_input = {"id": image["id"], "performer_ids": performer_ids}
+                stash.update_image(update_input)
+            log.info(f"Updated {len(image_list)} images from gallery {galleryID}.")
+
+    if MODE == "update":
+        log.info(f"Finished updating {imagetotal} images.")
+    elif MODE == "dryrun":
+        log.info(f"Found {imagetotal} eligible images. No changes were made.")
+
+    log.exit("Plugin exited normally.")
+
+
+if __name__ == '__main__':
+    main()
